7a23588d1b487976c49bfd23110f595120e90a0e
Javier Gonzalez <user@MacBook-Pro-de-Kertec.local>
2024-05-08 12:15:53 -0600

    Creation of Project
    First Commit

---
+++ Photos App Transition/Photos App Transition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photos App Transition
+//
+//  Created by User on 08/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photos App Transition/Photos App Transition/Photos_App_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_App_TransitionApp.swift
+//  Photos App Transition
+//
+//  Created by User on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Photos_App_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
